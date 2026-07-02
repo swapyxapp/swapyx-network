26193f0ca0b8b8558793fc0a677fff03647a5670
swapyxapp <bolikolu1@gmail.com>
2026-07-02 12:48:10 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import SwapWidget from "@/components/SwapWidget";
+import AnalyticsCard from "@/components/AnalyticsCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <SwapWidget />
+      <AnalyticsCard />
+    </>
+  );
+}
